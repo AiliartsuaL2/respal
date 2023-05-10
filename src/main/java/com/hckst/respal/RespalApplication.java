4cd1bdf7942e272caa6034acf1072ef88e40d5d4
ailiartsua <ailiartsual2@gmail.com>
2023-05-10 21:43:09 +0900

    Initial commit

---
+++ src/main/java/com/hckst/respal/RespalApplication.java
@@ -0,0 +1,13 @@
+package com.hckst.respal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hckst/respal/RespalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hckst.respal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
