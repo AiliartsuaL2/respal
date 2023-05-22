2ac2dc3498a4d371bda921e9b2538c89a7f84afb
Juho <ailiartsual2@gmail.com>
2023-05-22 14:14:59 +0900

    Swagger 설정 추가

--- src/main/java/com/hckst/respal/members/presentation/MembersController.java
+++ src/main/java/com/hckst/respal/members/presentation/MembersController.java
@@ -5,6 +5,12 @@ import com.hckst.respal.members.presentation.dto.MemberJoinDto;
 import com.hckst.respal.exception.dto.ApiErrorResponse;
 import com.hckst.respal.authentication.jwt.dto.Token;
 import com.hckst.respal.members.application.MembersService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,6 +27,7 @@ import java.util.concurrent.RejectedExecutionException;
 @Controller
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "회원", description = "회원 관련 api")
 public class MembersController {
     private final MembersService membersService;
     @GetMapping("/member/login")
@@ -28,6 +35,12 @@ public class MembersController {
         return "member/login.html";
     }
 
+
+    @Operation(summary = "로그인 메서드", description = "일반 이메일 로그인 메서드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "로그인 실패(올바르지 않은 사용자 정보)", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    })
     @PostMapping("/member/login")
     @ResponseBody
     public ResponseEntity<?> login(@RequestBody MemberJoinDto memberJoinDto){
@@ -42,6 +55,13 @@ public class MembersController {
                 .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+
+    @Operation(summary = "회원가입 메서드", description = "일반 이메일 회원가입 메서드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "회원가입 실패 (중복된 이메일)", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    })
     @PostMapping("/member/join")
     @ResponseBody
     public ResponseEntity<?> join(@RequestBody MemberJoinDto memberJoinDto){
