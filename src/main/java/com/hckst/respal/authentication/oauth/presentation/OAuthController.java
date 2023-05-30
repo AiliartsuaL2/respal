5ab0d56c9f5b22dcedcb9adca5622b244e984ad9
ailiartsua <ailiartsual2@gmail.com>
2023-05-31 00:44:07 +0900

    oauth redirect test용

--- src/main/java/com/hckst/respal/authentication/oauth/presentation/OAuthController.java
+++ src/main/java/com/hckst/respal/authentication/oauth/presentation/OAuthController.java
@@ -21,10 +21,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @Controller
 @RequestMapping("/oauth")
@@ -100,6 +103,8 @@ public class OAuthController {
                 .grantType(token.getGrantType())
                 .build();
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("http://localhost:3000/signup"));
         return ResponseEntity.ok(response);
     }
 }
