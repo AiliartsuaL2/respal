e3b5bd13e541c775db254354be2b51b744ab13af
LeeJuho <ailiartsual2@gmail.com>
2024-02-05 10:52:44 +0900

    fix : h2 의존성 scope 변경 런타임 -> 테스트

--- src/main/java/com/hckst/respal/RespalApplication.java
+++ src/main/java/com/hckst/respal/RespalApplication.java
@@ -2,13 +2,12 @@ package com.hckst.respal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @ConfigurationPropertiesScan("com.hckst.respal.config")
 @EnableAsync
 @EnableJpaRepositories
